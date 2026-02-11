5ae3978c213e737b946bbb542afee402cc365dd1
황준수 <hahark@naver.com>
2026-02-11 11:35:20 +0900

    feat: Add new CORS origin, create requirements.txt, and include sample uploaded images.

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ app = FastAPI(title="Community API - Task 2-1")
 # 0. 미들웨어 설정 (CORS)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://43.200.163.12:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
